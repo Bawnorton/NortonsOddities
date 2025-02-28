323c703771ddd06c82b5430239af6381dfd30838
Bawnorton <bawnorton@me.com>
2025-02-28 14:43:22 -0800

    render dark background

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Adds various QOL features to the TCG Modpack.
 
 ## Features
 - Insert Cards Button that automatically inserts all cards in your inventory into the collectors album if possible.
+- Renders dark background behind the category pages
--- src/main/java/com/bawnorton/tcgadditions/mixin/AlbumCategoryScreenMixin.java
+++ src/main/java/com/bawnorton/tcgadditions/mixin/AlbumCategoryScreenMixin.java
@@ -16,6 +16,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import team.tnt.collectorsalbum.client.screen.AlbumCategoryScreen;
 import team.tnt.collectorsalbum.common.AlbumCategory;
 import team.tnt.collectorsalbum.common.AlbumCategoryUiTemplate;
@@ -71,5 +73,13 @@ public abstract class AlbumCategoryScreenMixin extends AbstractContainerScreen<A
         if(toHighlight.isEmpty()) {
             TCGAdditions.SLOTS_TO_HIGHLIGHT.get().remove(category.identifier());
         }
+    }
+
+    @Inject(
+            method = "render",
+            at = @At("HEAD")
+    )
+    private void renderDarkBackground(GuiGraphics graphics, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        this.renderBackground(graphics);
     }
 }
